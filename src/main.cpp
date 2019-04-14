f27911b280cca910a03f7a3131b68b03f2444a06
Joshua Goldberg <joshuabg@terpmail.umd.edu>
2019-04-14 16:29:51 -0400

    Revert "Update wiring to control 2 motors with 1 set of PWM pins"
    This reverts commit dfd8cb3b52560fbe528c9ee3f14399013f3e549b.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
 #include <Arduino.h>
+#include <DRV8871.h>
+#include <DRV8871Quad.h>
 #include <Enes100.h>
 #include <NewPing.h>
+#include <Thread.h>
+#include <ThreadController.h>
+#include <TimerOne.h>
 
-// pin configuration (for Arduino Uno)
+// pin configuration (for Arduino Mega)
 // Motors need PWM pins
 
-// Right motors
-#define RIGHT_MOTOR_IN1 5
-#define RIGHT_MOTOR_IN2 3
+// Front Right
+#define FR_MOTOR_IN1 3
+#define FR_MOTOR_IN2 2
 
-// Left motors
-#define LEFT_MOTOR_IN1 9
-#define LEFT_MOTOR_IN2 6
+// Front Left
+#define FL_MOTOR_IN1 5
+#define FL_MOTOR_IN2 4
+
+// Back Right
+#define BR_MOTOR_IN1 7
+#define BR_MOTOR_IN2 6
+
+// Back Left
+#define BL_MOTOR_IN1 11
+#define BL_MOTOR_IN2 10
 
 #define SPEED1 255
 #define MAX_SPEED 255
 
 // APC220
 #define MARKER_ID 12
-#define APC_RX 7
-#define APC_TX 8
+#define APC_RX 8
+#define APC_TX 9
 
 // Ultrasonic sensors
-// TODO: Set up in 3 wire mode to save 2 pins
-#define RIGHT_TRIGGER_PIN 12
-#define RIGHT_ECHO_PIN 13
-#define LEFT_TRIGGER_PIN 2
-#define LEFT_ECHO_PIN 4
+#define RIGHT_TRIGGER_PIN 50
+#define RIGHT_ECHO_PIN 51
+#define LEFT_TRIGGER_PIN 52
+#define LEFT_ECHO_PIN 53
 #define MAX_DISTANCE 200
 
 #define ARENA_HEIGHT 2
@@ -127,6 +139,62 @@ void loop() {
   stop();
 }
 
+void moveForward(int speed) {
+  digitalWrite(FR_MOTOR_IN1, LOW);
+  analogWrite(FR_MOTOR_IN2, speed);
+
+  digitalWrite(FL_MOTOR_IN1, LOW);
+  analogWrite(FL_MOTOR_IN2, speed);
+
+  digitalWrite(BR_MOTOR_IN1, LOW);
+  analogWrite(BR_MOTOR_IN2, speed);
+
+  digitalWrite(BL_MOTOR_IN1, LOW);
+  analogWrite(BL_MOTOR_IN2, speed);
+}
+
+void moveBackward(int speed) {
+  digitalWrite(FR_MOTOR_IN2, LOW);
+  analogWrite(FR_MOTOR_IN1, speed);
+
+  digitalWrite(FL_MOTOR_IN2, LOW);
+  analogWrite(FL_MOTOR_IN1, speed);
+
+  digitalWrite(BR_MOTOR_IN2, LOW);
+  analogWrite(BR_MOTOR_IN1, speed);
+
+  digitalWrite(BL_MOTOR_IN2, LOW);
+  analogWrite(BL_MOTOR_IN1, speed);
+}
+
+void turnRight(int speed) {
+  digitalWrite(FR_MOTOR_IN2, LOW);
+  analogWrite(FR_MOTOR_IN1, speed);
+
+  digitalWrite(FL_MOTOR_IN1, LOW);
+  analogWrite(FL_MOTOR_IN2, speed);
+
+  digitalWrite(BR_MOTOR_IN2, LOW);
+  analogWrite(BR_MOTOR_IN1, speed);
+
+  digitalWrite(BL_MOTOR_IN1, LOW);
+  analogWrite(BL_MOTOR_IN2, speed);
+}
+
+void turnLeft(int speed) {
+  digitalWrite(FR_MOTOR_IN1, LOW);
+  analogWrite(FR_MOTOR_IN2, speed);
+
+  digitalWrite(FL_MOTOR_IN2, LOW);
+  analogWrite(FL_MOTOR_IN1, speed);
+
+  digitalWrite(BR_MOTOR_IN1, LOW);
+  analogWrite(BR_MOTOR_IN2, speed);
+
+  digitalWrite(BL_MOTOR_IN2, LOW);
+  analogWrite(BL_MOTOR_IN1, speed);
+}
+
 double getAngleToDest() {
   updateLocation();
   double deltaX = Enes100.location.x - Enes100.destination.x;
@@ -203,12 +271,17 @@ void goAroundObstacle() {
 }
 
 void stop() {
-  // TODO: this might be coasting instead of actually stopping
-  digitalWrite(RIGHT_MOTOR_IN1, LOW);
-  analogWrite(RIGHT_MOTOR_IN2, LOW);
+  digitalWrite(FR_MOTOR_IN1, LOW);
+  analogWrite(FR_MOTOR_IN2, LOW);
 
-  digitalWrite(LEFT_MOTOR_IN1, LOW);
-  analogWrite(LEFT_MOTOR_IN2, LOW);
+  digitalWrite(FL_MOTOR_IN1, LOW);
+  analogWrite(FL_MOTOR_IN2, LOW);
+
+  digitalWrite(BR_MOTOR_IN1, LOW);
+  analogWrite(BR_MOTOR_IN2, LOW);
+
+  digitalWrite(BL_MOTOR_IN1, LOW);
+  analogWrite(BL_MOTOR_IN2, LOW);
 }
 
 void turn(double targetAngle) {
@@ -250,36 +323,4 @@ void updateLocation() {
   // Enes100.print(", ");
   // Enes100.print(Enes100.location.theta);
   // Enes100.println(")");
-}
-
-void moveForward(int speed) {
-  digitalWrite(RIGHT_MOTOR_IN1, LOW);
-  analogWrite(RIGHT_MOTOR_IN2, speed);
-
-  digitalWrite(LEFT_MOTOR_IN1, LOW);
-  analogWrite(LEFT_MOTOR_IN2, speed);
-}
-
-void moveBackward(int speed) {
-  digitalWrite(RIGHT_MOTOR_IN2, LOW);
-  analogWrite(RIGHT_MOTOR_IN1, speed);
-
-  digitalWrite(LEFT_MOTOR_IN2, LOW);
-  analogWrite(LEFT_MOTOR_IN1, speed);
-}
-
-void turnRight(int speed) {
-  digitalWrite(RIGHT_MOTOR_IN2, LOW);
-  analogWrite(RIGHT_MOTOR_IN1, speed);
-
-  digitalWrite(LEFT_MOTOR_IN1, LOW);
-  analogWrite(LEFT_MOTOR_IN2, speed);
-}
-
-void turnLeft(int speed) {
-  digitalWrite(RIGHT_MOTOR_IN1, LOW);
-  analogWrite(RIGHT_MOTOR_IN2, speed);
-
-  digitalWrite(LEFT_MOTOR_IN2, LOW);
-  analogWrite(LEFT_MOTOR_IN1, speed);
 }
