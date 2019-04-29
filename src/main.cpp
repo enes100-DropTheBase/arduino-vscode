c9d54ade64db07a6e10fe102c1f05eefa873026c
Tom <tbrosh23@aol.com>
2019-04-29 13:44:02 -0400

    Add pH code

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,14 @@
 // Max ultrasonic distance in cm
 #define MAX_DISTANCE 200
 
+#define SensorPin A7            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00            //deviation compensate
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;   
+
 double getAngleToDest();
 double getDistToDest();
 void goAroundObstacle();
@@ -27,6 +35,7 @@ void updateLocation();
 void turn(double targetAngle);
 float pingLeft();
 float pingRight();
+double avergearray(int* arr, int number);
 
 NewPing rightSonar(RIGHT_TRIGGER_PIN, RIGHT_ECHO_PIN, MAX_DISTANCE);
 NewPing leftSonar(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN, MAX_DISTANCE);
@@ -204,6 +213,23 @@ void loop() {
           servo.write(servoAngle);          
           delay(10);      
         }
+
+        delay(5000);
+        unsigned long targetTime = millis() + 5000;
+        unsigned long samplingTime = millis();
+        float pHValue = 6;
+        while(millis() < targetTime) {
+          float voltage;
+          if(millis()-samplingTime > samplingInterval)
+          {
+              pHArray[pHArrayIndex++]=analogRead(SensorPin);
+              if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+              voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+              pHValue = 3.5*voltage+Offset;
+              samplingTime=millis();
+          }
+        }
+        Enes100.mission(pHValue);
   }
 
   stop();
@@ -413,4 +439,44 @@ float pingRight() {
   } else {
     return cm;
   }
+}
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
 }
